56851610842d282f67089ec554de45ddb05b986f
AlekseiTepljakov <alex@starspirals.net>
2021-03-04 11:24:04 +0200

    [Version] Added 4.26 ver. of plugin; docs update

---
+++ 4.26/UDPCommunication/Source/UDPCommunication/Public/UDPReceiver.cpp
@@ -0,0 +1,76 @@
+#include "UDPReceiver.h"
+
+AUDPReceiver::AUDPReceiver(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	ListenSocket = NULL;
+}
+
+void AUDPReceiver::Recv(const FArrayReaderPtr & ArrayReaderPtr, const FIPv4Endpoint & EndPt)
+{
+	if (!&ArrayReaderPtr) {
+		UE_LOG(LogTemp, Warning, TEXT("Cannot read array, nullptr returned."));
+		return;
+	}
+	
+	got_new_data = true;
+
+	if (UpdateInterest)
+		Archive(ArrayReaderPtr);
+}
+
+bool AUDPReceiver::StartUDPReceiver(const FString & SocketName, const int32 Port)
+{
+	FIPv4Endpoint Endpoint(FIPv4Address::Any, Port);
+	int32 BufferSize = 2 * 1024 * 1024;
+	ListenSocket = FUdpSocketBuilder(*SocketName).AsNonBlocking()
+		.AsReusable()
+		.BoundToEndpoint(Endpoint)
+		.WithReceiveBufferSize(BufferSize);
+	
+	FTimespan ThreadWaitTime = FTimespan::FromMilliseconds(100);
+	Receiver = new FUdpSocketReceiver(ListenSocket, ThreadWaitTime, TEXT("UDP Receiver"));
+	Receiver->OnDataReceived().BindUObject(this, &AUDPReceiver::Recv);
+	Receiver->Start();
+	return true;
+}
+
+void AUDPReceiver::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	delete Receiver;
+	Receiver = nullptr;
+
+	//Clear all sockets
+	if (ListenSocket)
+	{
+		ListenSocket->Close();
+		ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->DestroySocket(ListenSocket);
+	}
+}
+
+void AUDPReceiver::Archive(const FArrayReaderPtr & ArrayReaderPtr) {
+
+	FUDPData data;
+	*ArrayReaderPtr << data;
+
+	//Class Parameter Update function to set object ready for a query from blueprint
+	UpdateReceiverData(data);
+}
+
+void AUDPReceiver::UpdateReceiverData(FUDPData data)
+{
+	MyData = data;
+}
+
+FUDPData AUDPReceiver::GetData()
+{
+	UpdateInterest = true;	// Ready for new data
+	got_new_data = false;
+	return MyData;
+}
+
+bool AUDPReceiver::IsNewDataReady() {
+	return got_new_data;
+}
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Application example: https://recreation.ee/r/about/2018-11-ut/02/
 ## Installation and usage ##
 
 See these videos:
-* https://www.youtube.com/watch?v=db6Myy9R2OI (for UE4.17 and UE4.21 only atm!)
+* https://www.youtube.com/watch?v=db6Myy9R2OI (for UE4.17, UE4.21, and UE4.26 only atm!)
 * https://www.youtube.com/watch?v=Avj8T6RGyPo
 
 
@@ -19,6 +19,7 @@ Quick rundown:
 * The plugin is automatically activated and its contents can now be used.
 * Create an **Actor** blueprint. In the parent class field, choose either UDPReceiver or UDPSender depending on what you intend to use the plugin for.
 * Using the reference to the new blueprint instance, initialize the UDP socket in, e.g., Level Blueprint.
+* On the other hand, you can initialize the socket in the Actor blueprint itself using the On Begin Play event. This makes it much more convenient to use in the sense of modularity of actors without dependence on the Level Blueprint (which is considered a poor BP programming practice in general). To see an example of this in action, have a look at this repo: https://github.com/is-centre/Crane3D-simple-visualization-ue4
 * Use the receive/send functions, break the incoming data as needed, use it in your new Actor blueprint.
 
 ## Next steps ##
